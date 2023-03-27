ed648b416eecd2e8bda7a636f4f5ac6fc41ce2f2
Fabian Rojas <fabian.rojas.arce95@gmail.com>
2023-03-27 22:00:12 +0000

    feat: Se personaliza el usuario con nombre y apellido

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -3,8 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using RazorPagesPizza.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection");builder.Services.AddDbContext<RazorPagesPizzaAuth>(options =>
-    options.UseSqlServer(connectionString));builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
+    options.UseSqlServer(connectionString));
+    builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true)
+      .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
 // Add services to the container.
 builder.Services.AddRazorPages();
 
